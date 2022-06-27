f86e8093e6eae917155b8521a42210c2e790dfea
chenk <657301050@qq.com>
2022-06-27 11:32:31 +0800

    Update countDown.tsx

--- tools/countDown.tsx
+++ tools/countDown.tsx
@@ -1,72 +1,108 @@
-// 简易的倒计时, 接受并返回时间戳
-// todo 跟随系统跳秒
+// 跟随系统跳秒 done
+// 纠偏 done
+const useCountdown = (props?: CountdownPropsType) => {
+  const [stamp, setStamp] = useState<number>(0);
+  const ONE_SECEND = useRef<number>(1000);
 
-type TimerType = {
-  stamp: number|null;
-}
+  // 依赖 index次数计时器将其改为每1s执行的自纠正倒计时
+  const indexRef = useRef<number>(0);
 
-const useCounter = (props?:TimerType) => {
-  const [stamp, setStamp] = useState<number>(0)
-  const ONE_SECEND = useRef<number>(1000)
+  // 初始化props。stamp时和update时才需要设置这个prev
+  const prevTotalStampRef = useRef<number | null>(0);
+  // 变化中的stamp
+  const totalStampRef = useRef<number | null>(0);
 
-  const firstTimeRef = useRef<number>(new Date().getTime())
-  const indexRef = useRef<number>(0)
-  const myStampRef = useRef<number>(0)
-  const timerRef = useRef<Array<NodeJS.Timeout>>([])
+  // 调度真正开始执行时间
+  const startCountdownStampRef = useRef<number | null>(null);
 
-  const clearTimerRef = useCallback(() => {
-    indexRef.current = 0
-    firstTimeRef.current = new Date().getTime()
-    if (timerRef.current) {
-      timerRef.current.forEach((item) => clearTimeout(item))
-      timerRef.current = []
-    }
-  }, [])
+  // prefix 跟随系统跳秒
+  const prefixRef = useRef<number>(1000);
+
+  const beforeStarterAndInitValue = (timer: number) => {
+    totalStampRef.current = timer;
+    startCountdownStampRef.current = new Date().getTime();
+  };
 
   const counting = useCallback(() => {
-    console.log(`invoke task myStampRef.current:${myStampRef.current}， time ${indexRef.current}`)
-    indexRef.current += 1
-    const now = new Date().getTime()
-    const diff = now - (firstTimeRef.current + indexRef.current * ONE_SECEND.current)
-    const nextTask = ONE_SECEND.current - diff
-    myStampRef.current -= ONE_SECEND.current
-    setStamp(myStampRef.current)
-    // console.log(`myStampRef.current:${myStampRef.current}, nextTask:${nextTask}`)
+    if (startCountdownStampRef.current && totalStampRef.current) {
+      const fixPrefix = ONE_SECEND.current - prefixRef.current
+      indexRef.current += 1;
+      const now = new Date().getTime();
+      // 当前距离开始的间隔
+      const durations = now - startCountdownStampRef.current;
+      // 理论上递归后的执行间隔
+      const wishDurations = indexRef.current * ONE_SECEND.current - fixPrefix;
 
-    if (myStampRef.current <= 0) {
-      clearTimerRef()
-    } else {
-      const recursiveTimer = setTimeout(() => {
-        counting()
-      }, nextTask)
+      const diffdurations = durations - wishDurations;
+      if (
+        diffdurations > ONE_SECEND.current ||
+        diffdurations < -ONE_SECEND.current
+      ) {
+        console.log('fix')
+        indexRef.current = durations / ONE_SECEND.current;
+      }
 
-      timerRef.current.push(recursiveTimer)
+      const diff = now - (startCountdownStampRef.current + indexRef.current * ONE_SECEND.current - fixPrefix);
+      const nextTask = ONE_SECEND.current - diff;
+      const stamp = totalStampRef.current - durations;
+
+      setStamp(() => stamp);
+      if (stamp > 0) {
+        setTimeout(() => {
+          counting();
+        }, nextTask);
+      }
     }
-  }, [clearTimerRef])
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
-  const checkPropsStampAndStartTask = useCallback((timer?:number) => {
-    if (props?.stamp || timer) {
-      clearTimerRef()
-      myStampRef.current = props?.stamp || timer || 0
-      setStamp(props?.stamp || timer || 0)
-      setTimeout(counting, ONE_SECEND.current)
+  const checkPropsStampAndStartTask = useCallback((timer?: number) => {
+    if (timer) {
+      beforeStarterAndInitValue(timer);
+      setStamp(timer || 0);
+      prefixRef.current = ONE_SECEND.current - (new Date().getTime() % 1000);
+      setTimeout(counting, prefixRef.current);
     }
-  }, [clearTimerRef, counting, props?.stamp])
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
-  const updateStamp = (timer:number) => {
+  const updateStamp = (timer: number) => {
     if (typeof timer !== 'number') {
-      throw new Error('need post number')
+      throw new Error('need post number');
     }
-    clearTimerRef()
-    checkPropsStampAndStartTask(timer)
-  }
+    prevTotalStampRef.current = timer;
+    checkPropsStampAndStartTask(timer);
+  };
 
-  useEffectAfterQueryReady(() => {
-    checkPropsStampAndStartTask()
-  }, [checkPropsStampAndStartTask])
+  useEffect(() => {
+    checkPropsStampAndStartTask();
+  }, [checkPropsStampAndStartTask]);
+
+  useEffect(() => {
+    if (!prevTotalStampRef.current && props?.stampTimer) {
+      prevTotalStampRef.current = props.stampTimer;
+    }
+  }, [props?.stampTimer]);
 
   return {
     stamp,
     updateStamp,
+  };
+};
+
+const formatDuring = (stamp: number) => {
+  if (stamp <= 0) {
+    return '00:00:00';
   }
-}
+  // var days = parseInt(stamp / (1000 * 60 * 60 * 24));
+  const hour = parseInt(
+    `${(stamp % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)}`,
+    10
+  );
+  const minute = parseInt(`${(stamp % (1000 * 60 * 60)) / (1000 * 60)}`, 10);
+  const second = Math.floor((stamp % (1000 * 60)) / 1000);
+
+  return `${hour < 10 ? `0${hour}` : hour}:${
+    minute < 10 ? `0${minute}` : minute
+  }:${second < 10 ? `0${second}` : second}`;
+};
