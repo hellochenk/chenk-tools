b6d375ad829a22a5e55f3162e32b7b459c7bb6a5
chenk <657301050@qq.com>
2022-06-29 17:24:22 +0800

    Create recursive.tsx

---
+++ tools/recursive.tsx
@@ -0,0 +1,31 @@
+/* 蹦床函数 尾递归优化 递归 -> 遍历 */
+
+function tco(f) {
+  var value;
+  var active = false;
+  var accumulated = [];
+
+  return function accumulator() {
+    accumulated.push(arguments);
+    if (!active) {
+      active = true;
+      while (accumulated.length) {
+        value = f.apply(this, accumulated.shift());
+      }
+      active = false;
+      return value;
+    }
+  };
+}
+
+var sum = tco(function(x, y) {
+  if (y > 0) {
+    return sum(x + 1, y - 1)
+  }
+  else {
+    return x
+  }
+});
+
+sum(1, 100000)
+// 100001
